2b124cab19f6698391bd9ccea828401eb87fd522
starci183 <cuongnvtse160875@gmail.com>
2024-11-07 16:38:47 +0700

    add g

--- apps/broadcast-service/src/app.module.ts
+++ apps/broadcast-service/src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from "@nestjs/common"
 import { BroadcastPlacedItemsModule } from "./broadcast-placed-items"
 import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConfigModule } from "@nestjs/config"
+import { envConfig } from "@src/config"
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            load: [envConfig],
+            envFilePath: [".env.local"],
+            isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
             type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "Cuong123_A",
-            database: "cifarm",
+            host: envConfig().database.postgres.gameplay.host,
+            port: envConfig().database.postgres.gameplay.port,
+            username: envConfig().database.postgres.gameplay.user,
+            password: envConfig().database.postgres.gameplay.pass,
+            database: envConfig().database.postgres.gameplay.dbName,    
             autoLoadEntities: true,
             synchronize: true,
         }),
