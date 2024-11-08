c64f6dd934636f1dbd4519a6150182f45875c9e6
starci183 <cuongnvtse160875@gmail.com>
2024-11-08 16:38:31 +0700

    123123

--- apps/rest-api-gateway/src/main.ts
+++ apps/rest-api-gateway/src/main.ts
@@ -12,6 +12,6 @@ const bootstrap = async () => {
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup("/api", app, document)
-    await app.listen(envConfig().containers.authService.port)
+    await app.listen(envConfig().containers.restApiGatewat.port)
 }
 bootstrap()
