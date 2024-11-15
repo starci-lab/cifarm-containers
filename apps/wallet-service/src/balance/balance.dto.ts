f209592b9728d15276e8400f273b4ad7b83c7fea
starci183 <cuongnvtse160875@gmail.com>
2024-11-15 18:15:06 +0700

    123123

--- apps/wallet-service/src/balance/balance.dto.ts
+++ apps/wallet-service/src/balance/balance.dto.ts
@@ -4,7 +4,7 @@ import { IsInt, IsNumber } from "class-validator"
 
 export class GetBalanceRequest extends UserIdRequest {}
 
-//x
+//x2
 export class GetBalanceResponse {
     @IsInt()
     @ApiProperty({ example: 100, description: "The user's gold balance" })
