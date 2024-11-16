a96aea5859f214cf2beca83e18fd2406658ca88f
starci183 <cuongnvtse160875@gmail.com>
2024-11-16 21:09:25 +0700

    123123

--- src/utils/base.utils.ts
+++ src/utils/base.utils.ts
@@ -1,18 +1,19 @@
 import { NodeEnv, envConfig } from "@src/config"
 
+//
 export const getEnvValue = <ValueType = string>(values: {
-  development?: ValueType;
-  production?: ValueType;
+    development?: ValueType
+    production?: ValueType
 }): ValueType => {
     const { development, production } = values
     switch (envConfig().nodeEnv) {
-    case NodeEnv.Production:
-        return production
-    default:
-        return development
+        case NodeEnv.Production:
+            return production
+        default:
+            return development
     }
 }
- 
+
 export const waitFor = (observeVariable: boolean, delay: number = 100): Promise<void> => {
     return new Promise((resolve) => {
         const interval = setInterval(() => {
