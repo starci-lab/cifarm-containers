78a7cfbf99987d2a7f2da2c0f55d2e0de9378d10
Le Quoc Uy <lequocuyit@gmail.com>
2024-11-18 18:42:12 +0700

    Remove placeholder comment in e2e test for RestApiGatewayController

--- apps/rest-api-gateway/test/app.e2e-spec.ts
+++ apps/rest-api-gateway/test/app.e2e-spec.ts
@@ -7,8 +7,6 @@ const mockHealthcheckService = {
     doHealthcheck: jest.fn().mockReturnValue(of({ status: "ok" }))
 }
 
-// test
-
 describe("RestApiGatewayController (e2e)", () => {
     let app: INestApplication
 
