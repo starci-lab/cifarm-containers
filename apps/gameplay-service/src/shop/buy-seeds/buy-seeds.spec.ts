04c0a402b7950b6aa9b16925bbf695799934fbb8
Le Quoc Uy <lequocuyit@gmail.com>
2024-11-26 11:41:57 +0700

    refactor: consolidate seed and seed-static-data applications into a single seed application structure

--- apps/gameplay-service/src/shop/buy-seeds/buy-seeds.spec.ts
+++ apps/gameplay-service/src/shop/buy-seeds/buy-seeds.spec.ts
@@ -3,35 +3,30 @@ import { ConfigModule } from "@nestjs/config"
 import { Test } from "@nestjs/testing"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { envConfig, Network, SupportedChainKey } from "@src/config"
-import { CropEntity, InventoryEntity, PlacedItemEntity, UserEntity } from "@src/database"
-import { SeedDataModule, SeedDataService } from "@src/services"
-import { createDatabase } from "@src/utils"
+import { CropEntity, InventoryEntity, UserEntity } from "@src/database"
+import { SeedDataModule } from "@src/services"
 import * as path from "path"
-import { DataSource } from "typeorm"
-import { v4 } from "uuid"
+import { DataSource, DeepPartial } from "typeorm"
 import { BuySeedsModule } from "./buy-seeds.module"
+import { BuySeedsService } from "./buy-seeds.service"
 
 describe("BuySeedsService", () => {
     let dataSource: DataSource
-    let logger: Logger
-    let seedData: SeedDataService
+    const logger: Logger = new Logger("BuySeedsService:Test")
+    let service: BuySeedsService
+    const userEntity: DeepPartial<UserEntity> = {
+        username: "test_user",
+        chainKey: SupportedChainKey.Solana,
+        accountAddress: "0x123456789abcdef",
+        network: Network.Mainnet,
+        tokens: 50.5,
+        experiences: 10,
+        energy: 5,
+        level: 2,
+        golds: 1000
+    }
 
     beforeEach(async () => {
-        const mockDbName = v4()
-
-        console.log(path)
-        console.log(path.join(process.cwd(), ".env.local"))
-        console.log(process.env.GAMEPLAY_POSTGRES_HOST)
-
-        await createDatabase({
-            type: "postgres",
-            host: envConfig().database.postgres.gameplay.host,
-            port: envConfig().database.postgres.gameplay.port,
-            user: envConfig().database.postgres.gameplay.user,
-            pass: envConfig().database.postgres.gameplay.pass,
-            dbName: mockDbName
-        })
-
         const module = await Test.createTestingModule({
             imports: [
                 ConfigModule.forRoot({
@@ -41,31 +36,21 @@ describe("BuySeedsService", () => {
                 }),
                 TypeOrmModule.forRoot({
                     type: "postgres",
-                    host: envConfig().database.postgres.gameplay.host,
-                    port: envConfig().database.postgres.gameplay.port,
-                    username: envConfig().database.postgres.gameplay.user,
-                    password: envConfig().database.postgres.gameplay.pass,
-                    database: mockDbName,
+                    host: envConfig().database.postgres.gameplay.test.host,
+                    port: envConfig().database.postgres.gameplay.test.port,
+                    username: envConfig().database.postgres.gameplay.test.user,
+                    password: envConfig().database.postgres.gameplay.test.pass,
+                    database: envConfig().database.postgres.gameplay.test.dbName,
                     autoLoadEntities: true,
                     synchronize: true
                 }),
-                TypeOrmModule.forFeature([UserEntity, PlacedItemEntity]),
                 SeedDataModule,
                 BuySeedsModule
             ],
             providers: []
         }).compile()
-
-        logger = new Logger("BuySeedsService:Test")
-        seedData = module.get(SeedDataService)
         dataSource = module.get(DataSource)
-        // service = module.get(BuySeedsService)
-
-        console.log(envConfig().database.postgres.gameplay.host)
-
-        logger.debug("Seeding static data")
-        await seedData.seed(dataSource)
-        logger.debug("Seeded static data")
+        service = module.get(BuySeedsService)
     })
 
     it("Should happy case work", async () => {
@@ -73,28 +58,7 @@ describe("BuySeedsService", () => {
         await queryRunner.connect()
 
         try {
-            // Create user account
-            const user = await queryRunner.manager.save(UserEntity, {
-                username: "test_user",
-                chainKey: SupportedChainKey.Solana,
-                accountAddress: "0x123456789abcdef",
-                network: Network.Mainnet,
-                tokens: 50.5,
-                experiences: 10,
-                energy: 5,
-                level: 2,
-                golds: 1000
-            })
-
-            console.log("User created:", user)
-
-            // Fetch user to verify persistence
-            const savedUser = await queryRunner.manager.findOne(UserEntity, {
-                where: { id: user.id }
-            })
-
-            if (!savedUser) throw new Error("User not found in database")
-            console.log("Verified saved user:", savedUser)
+            const userBeforeBuydingSeed = await queryRunner.manager.save(UserEntity, userEntity)
 
             // Get carrot
             const crop = await queryRunner.manager.findOne(CropEntity, {
@@ -105,26 +69,90 @@ describe("BuySeedsService", () => {
             console.log("Crop:", crop)
 
             const buySeedRequest = {
-                id: crop.id,
-                userId: savedUser.id,
+                cropId: crop.id,
+                userId: userBeforeBuydingSeed.id,
                 quantity: 1
             }
 
             // Buy seeds
-            //await service.buySeeds(buySeedRequest)
+            await service.buySeeds(buySeedRequest)
+
+            // Check user golds
+            const userAfterBuyingSeed = await queryRunner.manager.findOne(UserEntity, {
+                where: { id: userBeforeBuydingSeed.id }
+            })
+
+            expect(userAfterBuyingSeed.golds).toBe(
+                userEntity.golds - crop.price * buySeedRequest.quantity
+            )
 
             // Check inventory
             const inventory = await queryRunner.manager.findOne(InventoryEntity, {
-                where: { userId: savedUser.id, inventoryType: { cropId: crop.id } },
+                where: { userId: userBeforeBuydingSeed.id, inventoryType: { cropId: crop.id } },
                 relations: {
                     inventoryType: true
                 }
             })
 
             expect(inventory.quantity).toBe(buySeedRequest.quantity)
-        } catch (error) {
-            logger.error("ERROR:", error)
-            throw error
+        } finally {
+            await queryRunner.release()
+        }
+    })
+
+    it("Should basic scenario work should buy 2 time, stack inventory", async () => {
+        const queryRunner = dataSource.createQueryRunner()
+        await queryRunner.connect()
+
+        try {
+            const userBeforeBuydingSeed = await queryRunner.manager.save(UserEntity, userEntity)
+
+            // Get carrot
+            const crop = await queryRunner.manager.findOne(CropEntity, {
+                where: { id: "carrot" }
+            })
+
+            if (!crop) throw new Error("Crop not found")
+            console.log("Crop:", crop)
+
+            // buySeedFirstRequest
+            const buySeedFirstRequest = {
+                cropId: crop.id,
+                userId: userBeforeBuydingSeed.id,
+                quantity: 2
+            }
+            await service.buySeeds(buySeedFirstRequest)
+
+            // buySeedSecondRequest
+            const buySeedSecondRequest = {
+                cropId: crop.id,
+                userId: userBeforeBuydingSeed.id,
+                quantity: 2
+            }
+            await service.buySeeds(buySeedSecondRequest)
+
+            // Check user golds
+            const userAfterBuyingSeed = await queryRunner.manager.findOne(UserEntity, {
+                where: { id: userBeforeBuydingSeed.id }
+            })
+
+            expect(userAfterBuyingSeed.golds).toBe(
+                userEntity.golds -
+                    (crop.price * buySeedFirstRequest.quantity +
+                        crop.price * buySeedSecondRequest.quantity)
+            )
+
+            // Check inventory
+            const inventory = await queryRunner.manager.findOne(InventoryEntity, {
+                where: { userId: userBeforeBuydingSeed.id, inventoryType: { cropId: crop.id } },
+                relations: {
+                    inventoryType: true
+                }
+            })
+
+            expect(inventory.quantity).toBe(
+                buySeedFirstRequest.quantity + buySeedSecondRequest.quantity
+            )
         } finally {
             await queryRunner.release()
         }
@@ -134,7 +162,7 @@ describe("BuySeedsService", () => {
         // Ensure the database is deleted after each test
         try {
             logger.debug("Deleting test database" + dataSource.options.database)
-            // await dataSource.dropDatabase()
+
             logger.debug("Deleted test database" + dataSource.options.database)
         } catch (error) {
             logger.debug("Error deleting test database", error)
--- apps/seed/src/app.service.ts
+++ apps/seed/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger, OnModuleInit } from "@nestjs/common"
+import { InjectDataSource } from "@nestjs/typeorm"
 import { SeedDataService } from "@src/services"
 import { DataSource } from "typeorm"
 
@@ -7,13 +8,20 @@ export class AppService implements OnModuleInit {
     private readonly logger = new Logger(AppService.name)
 
     constructor(
-        private readonly dataSource: DataSource,
+        @InjectDataSource("main")
+        private readonly dataSourceMain: DataSource,
+        @InjectDataSource("test")
+        private readonly dataSourceTest: DataSource,
         private readonly seedDataService: SeedDataService
     ) {}
 
     async onModuleInit() {
         this.logger.debug("SeedStaticData initialized")
-        await this.seedDataService.seed(this.dataSource)
+        this.logger.debug(this.dataSourceMain, this.dataSourceTest)
+        Promise.all([
+            await this.seedDataService.seed(this.dataSourceMain),
+            await this.seedDataService.seed(this.dataSourceTest)
+        ])
         this.logger.debug("SeedStaticData finished")
     }
 }
