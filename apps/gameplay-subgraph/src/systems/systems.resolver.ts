009e54cfdca1daac130b4fd5da6a02a80acbd8a1
Le Quoc Uy <lequocuyit@gmail.com>
2025-01-04 16:14:26 +0700

    feat: add new queries for crop randomness, animal randomness, starter, spin info, and energy regen time

--- apps/gameplay-subgraph/src/systems/systems.resolver.ts
+++ apps/gameplay-subgraph/src/systems/systems.resolver.ts
@@ -1,7 +1,7 @@
 import { Logger, UseInterceptors } from "@nestjs/common"
 import { Resolver, Query } from "@nestjs/graphql"
 import { SystemsService } from "./systems.service"
-import { Activities } from "@src/databases"
+import { Activities, AnimalRandomness, CropRandomness, EnergyRegenTime, SpinInfo, Starter } from "@src/databases"
 import { GraphQLCacheInterceptor } from "@src/cache"
 
 @Resolver()
@@ -17,4 +17,45 @@ export class SystemsResolver {
     async getActivities(): Promise<Activities> {
         return this.systemsService.getActivities()
     }
+
+    @UseInterceptors(GraphQLCacheInterceptor)
+    @Query(() => CropRandomness, {
+        name: "cropRandomness"
+    })
+    async getCropRandomness(): Promise<CropRandomness> {
+        return this.systemsService.getCropRandomness()
+    }
+
+    @UseInterceptors(GraphQLCacheInterceptor)
+    @Query(() => AnimalRandomness, {
+        name: "animalRandomness"
+    })
+    async getAnimalRandomness(): Promise<AnimalRandomness> {
+        return this.systemsService.getAnimalRandomness()
+    }
+
+    @UseInterceptors(GraphQLCacheInterceptor)
+    @Query(() => Starter, {
+        name: "starter"
+    })
+    async getStarter(): Promise<Starter> {
+        return this.systemsService.getStarter()
+    }
+
+    @UseInterceptors(GraphQLCacheInterceptor)
+    @Query(() => SpinInfo, {
+        name: "spinInfo"
+    })
+    async getSpinInfo(): Promise<SpinInfo> {
+        return this.systemsService.getSpinInfo()
+    }
+
+    @UseInterceptors(GraphQLCacheInterceptor)
+    @Query(() => EnergyRegenTime, {
+        name: "energyRegenTime"
+    })
+    async getEnergyRegenTime(): Promise<EnergyRegenTime> {
+        return this.systemsService.getEnergyRegenTime()
+    }
 }
+
--- apps/gameplay-subgraph/src/systems/systems.service.ts
+++ apps/gameplay-subgraph/src/systems/systems.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from "@nestjs/common"
 import { DataSource } from "typeorm"
-import { Activities, GameplayPostgreSQLService, SystemEntity, SystemId } from "@src/databases"
+import { Activities, AnimalRandomness, CropRandomness, EnergyRegenTime, GameplayPostgreSQLService, SpinInfo, Starter, SystemEntity, SystemId } from "@src/databases"
 
 @Injectable()
 export class SystemsService {
@@ -24,6 +24,81 @@ export class SystemsService {
                 }
             })
             return activities as Activities
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getCropRandomness(): Promise<CropRandomness> {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        try {
+            const { value : cropRandomness } = await queryRunner.manager.findOne(SystemEntity, {
+                where: {
+                    id: SystemId.CropRandomness
+                }
+            })
+            return cropRandomness as CropRandomness
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getAnimalRandomness(): Promise<AnimalRandomness> {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        try {
+            const { value : animalRandomness } = await queryRunner.manager.findOne(SystemEntity, {
+                where: {
+                    id: SystemId.AnimalRandomness
+                }
+            })
+            return animalRandomness as AnimalRandomness
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getStarter(): Promise<Starter> {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        try {
+            const { value : starter } = await queryRunner.manager.findOne(SystemEntity, {
+                where: {
+                    id: SystemId.Starter
+                }
+            })
+            return starter as Starter
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getSpinInfo(): Promise<SpinInfo> {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        try {
+            const { value : spinInfo } = await queryRunner.manager.findOne(SystemEntity, {
+                where: {
+                    id: SystemId.SpinInfo
+                }
+            })
+            return spinInfo as SpinInfo
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getEnergyRegenTime(): Promise<EnergyRegenTime> {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        try {
+            const { value : energyRegenTime } = await queryRunner.manager.findOne(SystemEntity, {
+                where: {
+                    id: SystemId.EnergyRegenTime
+                }
+            })
+            return energyRegenTime as EnergyRegenTime
         } finally {
             await queryRunner.release()
         }
