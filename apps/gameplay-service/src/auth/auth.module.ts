e9f467f849d1dbf07cb3de69295383362f8d93cc
starci183 <cuongnvtse160875@gmail.com>
2025-01-10 15:49:32 +0700

    4

--- apps/gameplay-service/src/auth/auth.module.ts
+++ apps/gameplay-service/src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { RefreshModule } from "./refresh"
 import { RequestMessageModule } from "./request-message"
 import { VerifySignatureModule } from "./verify-signature"
 import { GenerateSignatureModule } from "./generate-signature"
+import { BlockchainModule } from "@src/blockchain"
 
 @Module({
     imports: [
+        BlockchainModule.register({
+            isGlobal: true
+        }),
         GenerateSignatureModule,
         RequestMessageModule,
         VerifySignatureModule,
--- apps/gameplay-service/src/auth/generate-signature/generate-signature.module.ts
+++ apps/gameplay-service/src/auth/generate-signature/generate-signature.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common"
 import { GenerateSignatureService } from "./generate-signature.service"
 import { GenerateSignatureController } from "./generate-signature.controller"
 import { RequestMessageModule } from "../request-message"
-import { BlockchainModule } from "@src/blockchain"
  
 @Module({
-    imports: [BlockchainModule.register(), RequestMessageModule],
+    imports: [RequestMessageModule],
     controllers: [GenerateSignatureController],
     providers: [GenerateSignatureService],
     exports: [GenerateSignatureService]
