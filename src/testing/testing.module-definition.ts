1a2dcf884212600cc1d901bca4856b40f81fc7dd
Le Quoc Uy <lequocuyit@gmail.com>
2025-01-13 20:58:39 +0700

    refactor: remove unused testing module and service files

--- src/testing/testing.module-definition.ts
+++
@@ -1,8 +0,0 @@
-// File: axios.module-definition.ts
-
-import { ConfigurableModuleBuilder } from "@nestjs/common"
-import { TestingOptions } from "./testing.types"
-
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE } =
-    new ConfigurableModuleBuilder<TestingOptions>()
-        .build()
