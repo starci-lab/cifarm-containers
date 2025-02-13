130a8feb0c986f50e481c850d751bd74fe1827bd
starci183 <cuongnvtse160875@gmail.com>
2025-02-13 15:53:35 +0700

    4

--- src/common/types/inheritance.types.ts
+++ src/common/types/inheritance.types.ts
@@ -1,7 +1,6 @@
 import { ApiHideProperty, ApiProperty } from "@nestjs/swagger"
 import { IsOptional, IsString, IsUUID } from "class-validator"
 import { Socket } from "socket.io"
-import { DeepPartial, QueryRunner } from "typeorm"
 
 export class UserIdRequest {
     @IsUUID("4")
@@ -23,23 +22,6 @@ export interface UserIdWithSocketParams extends UserIdParams {
     socket: Socket
 }
 
-export interface EntityParams<TEntity> {
-    entity: DeepPartial<TEntity>
-}
-
-export interface EntityWithUserIdParams<TEntity> extends UserIdParams {
-    entity: DeepPartial<TEntity>
-}
-
-export interface ArrayEntityParams<TEntity> {
-    entities: Array<DeepPartial<TEntity>>
-}
-
-export interface ArrayEntityWithUserIdParams<TEntity> extends UserIdParams {
-    entities: Array<DeepPartial<TEntity>>
-    data: DeepPartial<TEntity>
-}
-
 export class CreatedResponse {
     @IsUUID("4")
     @ApiProperty({ example: "5a6919c3-6ae3-45de-81eb-f1bbb05a246d" })
@@ -49,20 +31,6 @@ export class CreatedResponse {
 export class ArrayResponse<TEntity> {
     @ApiProperty({ type: [Object] })
         items: Array<TEntity>
-}
-
-export class CreateRequest<TEntity> {
-    @ApiProperty({ type: Object })
-        item: DeepPartial<TEntity>
-}
-
-export class CreateWithUserIdRequest<TEntity> extends UserIdRequest {
-    @ApiProperty({ type: Object })
-        item: DeepPartial<TEntity>
-}
-
-export class QueryRunnerUserIdRequest extends UserIdRequest {
-    queryRunner: QueryRunner
 }
 
 export class CheckSufficientParams {
--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./base.utils"
 export * from "./math.utils"
-export * from "./database.utilts"
 export * from "./format.utils"
 export * from "./dayjs.utils"
