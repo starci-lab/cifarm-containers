7f3532ad5ba9588e5f5fd7e8fb819d956ccf4ef9
starci183 <cuongnvtse160875@gmail.com>
2025-03-20 18:29:13 +0700

    213

--- apps/cron-worker/src/fruit/fruit.worker.ts
+++ apps/cron-worker/src/fruit/fruit.worker.ts
@@ -2,12 +2,7 @@ import { FruitJobData } from "@apps/cron-scheduler"
 import { Processor, WorkerHost } from "@nestjs/bullmq"
 import { Logger } from "@nestjs/common"
 import { bullData, BullQueueName } from "@src/bull"
-import {
-    FruitCurrentState,
-    FruitSchema,
-    InjectMongoose,
-    PlacedItemSchema,
-} from "@src/databases"
+import { FruitCurrentState, InjectMongoose, PlacedItemSchema, PlacedItemType } from "@src/databases"
 import { DateUtcService } from "@src/date"
 import { CoreService, StaticService } from "@src/gameplay"
 import { Job } from "bullmq"
@@ -35,7 +30,9 @@ export class FruitWorker extends WorkerHost {
             .model<PlacedItemSchema>(PlacedItemSchema.name)
             .find({
                 placedItemType: {
-                    $in: this.staticService.placedItemTypes.map((placedItemType) => placedItemType.id),
+                    $in: this.staticService.placedItemTypes.map(
+                        (placedItemType) => placedItemType.id
+                    )
                 },
                 fruitInfo: {
                     $ne: null
@@ -48,68 +45,71 @@ export class FruitWorker extends WorkerHost {
                 }
             })
             .skip(skip)
-            .limit(take) 
-            .sort({ createdAt: "desc" }) 
+            .limit(take)
+            .sort({ createdAt: "desc" })
         const { hasCaterpillar, needFertilizer } = this.staticService.fruitInfo.randomness
         const promises: Array<Promise<void>> = []
         for (const placedItem of placedItems) {
             const promise = async () => {
                 const mongoSession = await this.connection.startSession()
                 try {
-                    const fruit = await this.connection
-                        .model<FruitSchema>(FruitSchema.name)
-                        .findById(placedItem.fruitInfo.fruit)
-                        .session(mongoSession)
+                    const placedItemType = this.staticService.placedItemTypes.find(
+                        (placedItemType) =>
+                            placedItemType.id === placedItem.placedItemType.toString()
+                    )
+                    if (!placedItemType) {
+                        throw new Error("Placed item type not found")
+                    }
+                    const fruit = this.staticService.fruits.find(
+                        (fruit) =>
+                            placedItemType.type === PlacedItemType.Fruit &&
+                            fruit.id === placedItemType.fruit.toString()
+                    )
+                    const { growthStages } = this.staticService.fruitInfo
+                    if (!fruit) {
+                        throw new Error("Fruit not found")
+                    }
                     // Add time to the seed growth
                     const updatePlacedItem = () => {
                         // return if the current stage is already max stage
-                        if (
-                            placedItem.fruitInfo.currentStage >= fruit.growthStages - 1
-                        ) {
+                        if (placedItem.fruitInfo.currentStage >= growthStages - 1) {
                             return
                         }
                         // add time to the seed growth
                         placedItem.fruitInfo.currentStageTimeElapsed += time
                         if (
-                            placedItem.fruitInfo.currentStageTimeElapsed <
-                                fruit.growthStageDuration
+                            placedItem.fruitInfo.currentStageTimeElapsed < fruit.growthStageDuration
                         ) {
                             return
                         }
                         // deduct the time elapsed from the current stage time elapsed
-                        placedItem.fruitInfo.currentStageTimeElapsed -=
-                                fruit.growthStageDuration
+                        placedItem.fruitInfo.currentStageTimeElapsed -= fruit.growthStageDuration
                         // increment the current stage
                         placedItem.fruitInfo.currentStage += 1
 
                         // if the current stage is less than max stage - 3, check if need water
-                        if (placedItem.fruitInfo.currentStage <= fruit.growthStages - 3) {
+                        if (placedItem.fruitInfo.currentStage <= growthStages - 3) {
                             if (Math.random() < needFertilizer) {
-                                placedItem.fruitInfo.currentState =
-                                        FruitCurrentState.NeedFertilizer
+                                placedItem.fruitInfo.currentState = FruitCurrentState.NeedFertilizer
                             }
                             placedItem.fruitInfo.currentStageTimeElapsed = 0
                             return
                         }
 
                         // if the current stage is max stage - 2, check if weedy or infested
-                        if (placedItem.fruitInfo.currentStage === fruit.growthStages - 2) {
+                        if (placedItem.fruitInfo.currentStage === growthStages - 2) {
                             if (Math.random() < hasCaterpillar) {
-                                placedItem.fruitInfo.currentState =
-                                FruitCurrentState.IsInfested
+                                placedItem.fruitInfo.currentState = FruitCurrentState.IsInfested
                             }
                             return
                         }
                         // else, the fruit is fully matured
-                        if (
-                            placedItem.fruitInfo.currentState ===
-                                    FruitCurrentState.IsInfested
-                        ) {
-                            placedItem.fruitInfo.harvestQuantityRemaining =
-                                    Math.floor((fruit.minHarvestQuantity + fruit.maxHarvestQuantity) / 2)
+                        if (placedItem.fruitInfo.currentState === FruitCurrentState.IsInfested) {
+                            placedItem.fruitInfo.harvestQuantityRemaining = Math.floor(
+                                (fruit.minHarvestQuantity + fruit.maxHarvestQuantity) / 2
+                            )
                         } else {
-                            placedItem.fruitInfo.harvestQuantityRemaining =
-                                    fruit.maxHarvestQuantity
+                            placedItem.fruitInfo.harvestQuantityRemaining = fruit.maxHarvestQuantity
                         }
                         const chance = this.coreService.computeFruitQualityChance({
                             placedItemFruit: placedItem,
