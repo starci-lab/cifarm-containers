8aa3ceea98d32e416e3d8dfeb14edd0f6891bb77
starci183 <cuongnvtse160875@gmail.com>
2025-03-23 02:52:33 +0700

    123

--- apps/io-gameplay/src/gameplay/user/user.module.ts
+++ apps/io-gameplay/src/gameplay/user/user.module.ts
@@ -2,9 +2,8 @@ import { Module } from "@nestjs/common"
 import { UserConsumer } from "./user.consumer"
 import { UserGateway } from "./user.gateway"
 import { AuthModule } from "../auth"
-import { UserService } from "./user.service"
 @Module({
     imports: [ AuthModule ],
-    providers: [ UserGateway, UserConsumer, UserService ]
+    providers: [ UserGateway, UserConsumer ]
 })
 export class UserModule {}
