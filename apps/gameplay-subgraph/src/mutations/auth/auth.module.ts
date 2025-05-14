196fa9ef167c28deaefcad2f347cfd0098f8f151
starci183 <cuongnvtse160875@gmail.com>
2025-05-14 14:29:50 +0700

    fix bugs

--- apps/gameplay-subgraph/src/mutations/auth/auth.module.ts
+++ apps/gameplay-subgraph/src/mutations/auth/auth.module.ts
@@ -3,10 +3,9 @@ import { RefreshModule } from "./refresh"
 import { RequestMessageModule } from "./request-message"
 import { VerifySignatureModule } from "./verify-signature"
 import { GenerateSignatureModule } from "./generate-signature"
-import { AuthenticateGoogleModule } from "./authenticate-google"
+
 @Module({
     imports: [
-        AuthenticateGoogleModule,
         GenerateSignatureModule,
         RequestMessageModule,
         VerifySignatureModule,
--- apps/gameplay-subgraph/src/mutations/auth/authenticate-google/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./authenticate-google.dto"
-export * from "./authenticate-google.module"
-export * from "./authenticate-google.service"
