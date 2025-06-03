caf80c0ac1665e6ab1c199585800b6717db4292d
starci183 <cuongnvtse160875@gmail.com>
2025-06-03 17:18:51 +0700

    fix rpc use helius

--- src/blockchain/rpcs/solana/solana.ts
+++ src/blockchain/rpcs/solana/solana.ts
@@ -1,11 +1,11 @@
 import { Network } from "@src/env"
-import { clusterApiUrl } from "@solana/web3.js"
+// import { clusterApiUrl } from "@solana/web3.js"
 
 export const solanaHttpRpcUrl = (network: Network) => {
     let rpcUrl = ""
     switch (network) {
     case Network.Mainnet: {
-        rpcUrl = clusterApiUrl("mainnet-beta")
+        rpcUrl = "https://mainnet.helius-rpc.com/?api-key=195f7f46-73d5-46df-989e-9d743bf3caad"
         break
     }
     case Network.Testnet: {
